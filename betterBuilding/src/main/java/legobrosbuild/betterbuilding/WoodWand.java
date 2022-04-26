256d1994edb9b081b552c9066c6e1c6bb6bb651e
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-04-25 18:36:17 -0700

    marked up your code - let me know if you want me to try to fix the networking stuff

--- betterBuilding/src/main/java/legobrosbuild/betterbuilding/WoodWand.java
+++ betterBuilding/src/main/java/legobrosbuild/betterbuilding/WoodWand.java
@@ -9,14 +9,18 @@ import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraft.util.math.BlockPos;
+
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -49,10 +53,9 @@ public class WoodWand extends Item {
 
         HitResult hit = playerEntity.raycast(5, 0, false);
 
-        Block[] plankList = { Blocks.CRIMSON_PLANKS, Blocks.WARPED_PLANKS, Blocks.DARK_OAK_PLANKS, Blocks.ACACIA_PLANKS,
-                Blocks.JUNGLE_PLANKS, Blocks.OAK_PLANKS, Blocks.BIRCH_PLANKS, Blocks.SPRUCE_PLANKS };
-        Block[] logList = { Blocks.CRIMSON_STEM, Blocks.WARPED_STEM, Blocks.DARK_OAK_LOG, Blocks.ACACIA_LOG,
-                Blocks.JUNGLE_LOG, Blocks.OAK_LOG, Blocks.BIRCH_LOG, Blocks.SPRUCE_LOG };
+        // plankList made obsolete by tag system + registry
+        List<Block> logList = List.of(Blocks.CRIMSON_STEM, Blocks.WARPED_STEM, Blocks.DARK_OAK_LOG, Blocks.ACACIA_LOG,
+                Blocks.JUNGLE_LOG, Blocks.OAK_LOG, Blocks.BIRCH_LOG, Blocks.SPRUCE_LOG);
 
         switch (hit.getType()) {
             case MISS:
@@ -65,34 +68,39 @@ public class WoodWand extends Item {
 
                 System.out.println(block);
 
-                if (block.toString().contains("planks")) { // Makes sure the block is a plank
+                // Update: use block tags instead of list. Helps with mod compatibility
+                if (blockState.isIn(BlockTags.PLANKS)) { // Makes sure the block is a plank: new, modernized version
 
-                    String selectedBlock = plankList[woodNum].toString(); // Full name of the selected block
+                    String selectedBlock = Registry.BLOCK.getId(block).getPath(); // Registry id of the block (without namespace, e.g. "oak_planks")
                     Pattern pattern = Pattern.compile("(\\w)+_(\\w)+"); // REGEX
                     Matcher matcher = pattern.matcher(selectedBlock); // REGEX match
 
-                    if (matcher.find() == true) {
+                    if (matcher.find()) {
                         String result = matcher.group();
-                        String sel = "Currently Selected: " + result + "    Locked: " + status; // Selected block
+                        String sel = "Currently Selected: " + result + " Locked: " + status; // Selected block
                         playerEntity.sendMessage(Text.of(sel), true);
                     }
 
-                    world.setBlockState(blockPos, plankList[woodNum].getDefaultState()); // Sets block
+                    world.setBlockState(blockPos, block.getDefaultState()); // Sets block
 
                     woodNum++;
                 }
 
                 break;
         }
-        ServerPlayNetworking.registerGlobalReceiver(BetterBuildingClient.LOCK_WAND_ID, (server, player, handler, buf, responseSender) -> { // This line [17:41:22] [Server thread/ERROR] (Minecraft) Failed to handle packet net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket@271963c2, suppressing error
-            //java.lang.RuntimeException: Cannot load class legobrosbuild.betterbuilding.client.BetterBuildingClient in environment type SERVER
-
-            int val = buf.readInt();
-            System.out.println(val);
-           server.execute(() -> {
-            player.sendMessage(new LiteralText("Packet received"), false);
-           });
-        });
+        /* Wrong place for this. Try BetterBuilding.onInitialize? This will run every time the player uses the item,
+           and will create extra handlers. Also, the player won't be able to lock the item before the player uses it.
+        */
+//        ServerPlayNetworking.registerGlobalReceiver(BetterBuildingClient.LOCK_WAND_ID, (server, player, handler, buf, responseSender) -> { // This line [17:41:22] [Server thread/ERROR] (Minecraft) Failed to handle packet net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket@271963c2, suppressing error
+//            //java.lang.RuntimeException: Cannot load class legobrosbuild.betterbuilding.client.BetterBuildingClient in environment type SERVER
+//            // Miles says: store LOCK_WAND_ID in BetterBuilding.java, not BetterBuildingClient.java
+//
+//            int val = buf.readInt();
+//            System.out.println(val);
+//           server.execute(() -> {
+//            player.sendMessage(new LiteralText("Packet received"), false);
+//           });
+//        });
 
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
