f63b99b99ab93c5e63e799f5674d7aca85f2f675
Yodaman07 <76266292+Yodaman07@users.noreply.github.com>
2022-04-30 16:58:20 -0700

    Binding now works (on singleplayer)

--- betterBuilding/src/main/java/legobrosbuild/betterbuilding/mixin/BindingMixin.java
+++ betterBuilding/src/main/java/legobrosbuild/betterbuilding/mixin/BindingMixin.java
@@ -5,6 +5,8 @@ package legobrosbuild.betterbuilding.mixin;
 
 import legobrosbuild.betterbuilding.BetterBuilding;
 
+
+import legobrosbuild.betterbuilding.WoodWand;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -23,16 +25,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(Item.class)
-public class BindingMixin {
+public abstract class BindingMixin {
+
+    private static WoodWand boundWand = BetterBuilding.WOOD_WAND;
     @Inject(at = @At("HEAD"), method = "use", cancellable = true)
     private void init(World world, PlayerEntity player, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+
           Item bound = BetterBuilding.boundWand.get(player.getUuid());
           Identifier id = Registry.ITEM.getId(bound);
           //Check that item in the method is the bound Item
           Item isBoundItem = player.getStackInHand(hand).getItem();
 
-          if (id == Registry.ITEM.getId(isBoundItem)) {
-              player.sendMessage(new LiteralText("YOU HAVE CLICKED ON THE BOUND WAND"), false); //Run code from the "WOOD WAND" Class
+          if (id == Registry.ITEM.getId(isBoundItem) && !world.isClient()) {
+              player.sendMessage(new LiteralText("YOU HAVE CLICKED ON THE BOUND WAND"), false); //Run code from the "WOOD WAND" Class // DOESN'T WORK ON SERVER
+              boundWand.use(world, player, hand);
+
           }
     }
 }
