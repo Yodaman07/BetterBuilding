beb00fa3086010a49a583d97e35deda4c0b19252
Yodaman07 <76266292+Yodaman07@users.noreply.github.com>
2022-05-03 18:03:31 -0700

    Added a settings menu

---
+++ betterBuilding/src/main/java/legobrosbuild/betterbuilding/BBSettingsScreen.java
@@ -0,0 +1,45 @@
+package legobrosbuild.betterbuilding;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ScreenTexts;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.gui.widget.ToggleButtonWidget;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+
+import static legobrosbuild.betterbuilding.WoodWand.useDiagonals;
+
+public class BBSettingsScreen extends GameOptionsScreen {
+
+    public BBSettingsScreen(Screen parent, GameOptions options) {
+        super(parent, options, new TranslatableText("options.bbSettings.title"));
+    }
+
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        //Code for the BetterBuilding settings screen goes here
+
+        super.renderBackground(matrices);
+        BBSettingsScreen.drawCenteredText(new MatrixStack(), super.textRenderer, "UseDiagonals",this.width/2-80, this.height/6, 0xffffff);
+        BBSettingsScreen.drawCenteredText(new MatrixStack(), super.textRenderer, "OtherSetting",this.width/2+80, this.height/6, 0xffffff);
+
+
+        super.addDrawableChild(CyclingButtonWidget.onOffBuilder(new LiteralText(""), new LiteralText("")).build(this.width / 2 - 155, this.height / 6 - 12 + 24, 150, 20, new LiteralText("ERROR"), (button, value) -> {
+            useDiagonals =! useDiagonals;
+            super.clearChildren();
+        })).setMessage(new LiteralText(useDiagonals ? "True" : "False").formatted(useDiagonals? Formatting.GREEN: Formatting.RED));
+
+        super.addDrawableChild(new ButtonWidget(this.width / 2 + 5, this.height / 6 -12 + 24, 150, 20, new TranslatableText("bbSettings.otherOption"), button1 -> System.out.println("Toggled")));
+
+        super.addDrawableChild(new ButtonWidget(this.width /2 - 100,this.height / 6 - 12 + 54, 200, 20, ScreenTexts.DONE, button -> this.client.setScreen(this.parent)));
+//        super.renderBackgroundTexture(10); //Background Toggle
+        super.render(matrices, mouseX, mouseY, delta);
+
+    }
+
+}
--- betterBuilding/src/main/java/legobrosbuild/betterbuilding/mixin/BindingMixin.java
+++ betterBuilding/src/main/java/legobrosbuild/betterbuilding/mixin/BindingMixin.java
@@ -8,7 +8,6 @@ import legobrosbuild.betterbuilding.BetterBuilding;
 
 import legobrosbuild.betterbuilding.WoodWand;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -19,7 +18,6 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
